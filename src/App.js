1fe7b2f19b37cf7a0f77d3dd202549f16f3b64b8
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2024-08-19 10:03:54 +0100

    add state for the step along with the onClick for each button

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 const messages = [
   'Learn React ⚛️',
   'Apply for jobs 💼',
@@ -5,7 +7,14 @@ const messages = [
 ];
 
 export default function App() {
-  const step = 1;
+  const [step, setStep] = useState(1);
+
+  const handlePrevious = () => {
+    setStep(step - 1);
+  };
+  const handleNext = () => {
+    setStep(step + 1);
+  };
 
   return (
     <div className="steps">
@@ -20,10 +29,14 @@ export default function App() {
       </p>
 
       <div class="buttons">
-        <button style={{ backgroundColor: '#7950f2', color: '#fff' }}>
+        <button
+          style={{ backgroundColor: '#7950f2', color: '#fff' }}
+          onClick={handlePrevious}>
           previous
         </button>
-        <button style={{ backgroundColor: '#7950f2', color: '#fff' }}>
+        <button
+          style={{ backgroundColor: '#7950f2', color: '#fff' }}
+          onClick={handleNext}>
           next
         </button>
       </div>
