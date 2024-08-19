e0b843183c65b44a74f51009554f9a897978b3f5
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2024-08-19 09:58:35 +0100

    Created simple UI to display for the user

--- src/App.js
+++ src/App.js
@@ -1,3 +1,32 @@
+const messages = [
+  'Learn React ⚛️',
+  'Apply for jobs 💼',
+  'Invest your new income 🤑',
+];
+
 export default function App() {
-  return <div>Hello React</div>;
+  const step = 1;
+
+  return (
+    <div className="steps">
+      <div class="numbers">
+        <div className="active">1</div>
+        <div>2</div>
+        <div>3</div>
+      </div>
+
+      <p class="message">
+        Step {step}: {messages[step - 1]}
+      </p>
+
+      <div class="buttons">
+        <button style={{ backgroundColor: '#7950f2', color: '#fff' }}>
+          previous
+        </button>
+        <button style={{ backgroundColor: '#7950f2', color: '#fff' }}>
+          next
+        </button>
+      </div>
+    </div>
+  );
 }
